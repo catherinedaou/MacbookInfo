42c4723adb64ed774b556870f581041f7463bf9d
Catherine Daou <catherine.daou@gmail.com>
2021-12-16 12:57:35 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,28 @@
+playSound("assets/category_app/perfect_app_button_2.mp3");
+onEvent("beginningButton", "click", function( ) {
+  setScreen("beginningScreen");
+  playSound("assets/category_human/character_calvin_amazing_1.mp3");
+});
+onEvent("costButton", "click", function( ) {
+  setScreen("types/costScreen");
+  playSound("assets/category_bell/vibrant_cash_register_open_positive_game_open.mp3");
+});
+onEvent("salesButton", "click", function( ) {
+  setScreen("salesScreen");
+});
+onEvent("homeButton1", "click", function( ) {
+  setScreen("homeScreen");
+});
+onEvent("homeButton2", "click", function( ) {
+  setScreen("homeScreen");
+});
+onEvent("homeButton3", "click", function( ) {
+  setScreen("homeScreen");
+});
+onEvent("nextpageButton1", "click", function( ) {
+  setScreen("types/costScreen");
+  playSound("assets/category_bell/vibrant_cash_register_open_positive_game_open.mp3");
+});
+onEvent("nextpageButton2", "click", function( ) {
+  setScreen("salesScreen");
+});
